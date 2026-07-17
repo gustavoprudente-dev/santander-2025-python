ed04743f351c2d95279fe8f6f61d454505818290
Gustavo Mendonça Prudente <gustavomprudente@gmail.com>
2026-07-17 16:21:30 -0300

    mandando Módulos 1 e 2

---
+++ Módulo 1/variaveis_constantes.py
@@ -0,0 +1,5 @@
+idade, nome = 23, 'Gustavo'
+META_MENSAL = [3000, 20000]
+
+print(idade, nome, META_MENSAL)
+print(META_MENSAL)
---
+++ Módulo 2/operadores_aritmeticos.py
@@ -0,0 +1,16 @@
+p1 = 5
+p2 = 2
+
+print(p1 + p2)
+print(p1 - p2)
+print(p1 / p2)
+print(p1 // p2)
+print(p1 * p2)
+print(p1 % p2)
+print(p1 ** p2)
+
+x = (10 + 5) * 4
+y = 3 * p1 // p2
+
+print(x)
+print(y)
